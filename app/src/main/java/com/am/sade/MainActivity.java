e073c56bee4c0fdd64a93a925a3785dce0cf051f
Renan Lima <renan.n.lima@hotmail.com>
2018-02-12 08:44:47 +0800

    1 commit

--- app/src/main/java/com/am/sade/MainActivity.java
+++ app/src/main/java/com/am/sade/MainActivity.java
@@ -2,20 +2,27 @@ package com.am.sade;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.security.SecurityPermission;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, AdapterView.OnItemSelectedListener{
 
-    Spinner uf;
-    Spinner cid;
-    Spinner pme;
-    Spinner plano;
-    Spinner temPlano;
+    private Spinner uf;
+    private Spinner cid;
+    private Spinner pme;
+    private Spinner plano;
+    private Spinner temPlano;
+    private CheckBox bronze;
+    private CheckBox prata;
+    private CheckBox ouro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +49,51 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapterTemPlano = ArrayAdapter.createFromResource(this,R.array.TemPlano,android.R.layout.simple_spinner_item);
         temPlano.setAdapter(adapterTemPlano);
 
+        bronze = (CheckBox)findViewById(R.id.checkBoxBronze);
+        prata = (CheckBox)findViewById(R.id.checkBoxPrata);
+        ouro = (CheckBox)findViewById(R.id.checkBoxOuro);
+
+        bronze.setOnCheckedChangeListener(this);
+        prata.setOnCheckedChangeListener(this);
+
+        bronze.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"Ok",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 
-    AdapterView.OnItemSelectedListener cidade = new AdapterView.OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.checkBoxBronze:
+                Toast.makeText(getApplicationContext(),"ok",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.checkBoxPrata:
 
-
+                break;
+            case R.id.
         }
+    }
 
-        @Override
-        public void onNothingSelected(AdapterView<?> adapterView) {
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position){
+            case 0:
+                /*Faz alguma coisa*/
+                break;
         }
-    };
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
